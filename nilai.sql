32b52d48326a85e939887cab1fd3ea71b6b7992d
Mas Yoons <mas.yoons@gmail.com>
2025-09-18 20:36:59 +0700

    Create 'nilai' table with specified columns

---
+++ nilai.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `nilai` (
+  `idn` int(10) NOT NULL AUTO_INCREMENT,
+  `huruf` varchar(10) DEFAULT NULL,
+  `angka` varchar(10) DEFAULT NULL,
+  `stt` int(5) DEFAULT '1',
+  PRIMARY KEY (`idn`)
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
